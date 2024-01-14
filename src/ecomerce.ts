bd30186638fc898c5ab4b96e147a8b2475f9c1b0
wa5 <waseemslyar@gmail.com>
2024-01-14 12:56:50 +0530

    updated

--- src/ecomerce.ts
+++ src/ecomerce.ts
@@ -1,5 +1,18 @@
 export const ecomerce={
-    productNames:()=>{
+    productNames:(name?:string)=>{
+        if(name=='name'){
+            return ["Wheel powder","Tied Powder","Arial Powder"]
+        }
+        if(name=='image'){
+            return ["Wheel powder","Tied Powder","Arial Powder"]
+        }
+        
+        return ["Wheel powder","Tied Powder","Arial Powder"]
+
+
+    },
+    fruitesNames:()=>{
         return ["apple","mango","banana"]
     }
+
 }
